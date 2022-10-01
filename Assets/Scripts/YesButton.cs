8e1b225629c1e4c8fbecedf873f9a0795cbaaab1
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2022-10-01 18:52:18 -0500

    game over button resets scene

---
+++ Assets/Scripts/YesButton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class YesButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private bool mouse_over = false;
+    CanvasGroup canvasGroup;
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouse_over)
+        {
+            canvasGroup.alpha = 1.0f;
+            if(Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+        else
+        {
+            canvasGroup.alpha = 0.0f;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+    }
+}
