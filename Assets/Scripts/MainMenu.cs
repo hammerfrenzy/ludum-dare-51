505ba501cdb97cbc57c44c7afbce1eb72c716481
Wyatt Beavers <hammerfrenzy@gmail.com>
2022-10-02 12:23:55 -0500

    Add keyboard shortcuts to Main Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayGame();
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ShowInstructions();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ReturnToMain();
+        }
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("SampleScene");
